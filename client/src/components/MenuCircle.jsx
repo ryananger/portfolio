13f924af247cf50e521b4d2e8cbc6ea4ca82fbbc
ryananger <nightcrawling@gmail.com>
2024-02-08 02:11:07 -0500

    shine remove

--- client/src/components/MenuCircle.jsx
+++ client/src/components/MenuCircle.jsx
@@ -59,9 +59,7 @@ const MenuCircle = function({show, coords, page, index}) {
   useEffect(()=>{}, [style]);
 
   return (
-    <div ref={el} className='menuItem circle grow anchor' style={{...style, width: width, margin: `${-width/2}px`}} onClick={()=>{st.setPage(page)}}>
-      <div className='circle shine float'/>
-    </div>
+    <div ref={el} className='menuItem circle grow anchor' style={{...style, width: width, margin: `${-width/2}px`}} onClick={()=>{st.setPage(page)}}/>
   );
 };
 
